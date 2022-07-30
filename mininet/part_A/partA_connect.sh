a48812a71b570a42fb89972120d00e7a8379984a
Xinyang <x662li@uwaterloo.ca>
2022-07-29 22:22:21 -0400

    fix error

--- mininet/part_A/partA_connect.sh
+++ mininet/part_A/partA_connect.sh
@@ -16,13 +16,13 @@ ovs-vsctl set bridge s6 protocols=OpenFlow13
 
 # print protocol each switch supports
 for switch in s0 s1 s2 s3 s4 s6;
-do 
+do
     protos=$(ovs-vsctl get bridge $switch protocols)
-    echo "switch $switch supports $protos"
+    echo "Switch $switch supports $protos"
 done
 
-# abbr for command
-ofctl = 'ovs-ofctl -O OpenFlow13'
+# Avoid having to write "-O OpenFlow13" before all of your ovs-ofctl commands.
+ofctl='ovs-ofctl -O OpenFlow13'
 
 # h0 -> h2
 $ofctl add-flow s0 \
@@ -38,7 +38,7 @@ $ofctl add-flow s0 \
 # h1 -> h4
 $ofctl add-flow s1 \
     in_port=1,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:0D:01:00:03,output=3
-$offctl add-flow s2 \
+$ofctl add-flow s2 \
     in_port=3,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:FE:00:04,mod_dl_dst:0A:00:0D:FE:00:02,output=4
 $ofctl add-flow s3 \
     in_port=2,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0E:01:00:03,mod_dl_dst:0A:00:0E:FE:00:02,output=3
@@ -55,16 +55,23 @@ $ofctl add-flow s1 \
     in_port=3,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
 
 # h3 -> h6
-$offctl add-flow s3 \
+$ofctl add-flow s3 \
     in_port=1,ip,nw_src=10.0.3.2,nw_dst=10.0.6.2,actions=mod_dl_src:0A:00:0F:01:00:04,mod_dl_dst:0A:00:0F:FE:00:02,output=4
-$offctl add-flow s6 \
+$ofctl add-flow s6 \
     in_port=2,ip,nw_src=10.0.3.2,nw_dst=10.0.6.2,actions=mod_dl_src:0A:00:06:01:00:01,mod_dl_dst:0A:00:06:02:00:00,output=1
 # h6 -> h3
-$offctl add-flow s6 \
+$ofctl add-flow s6 \
     in_port=1,ip,nw_src=10.0.6.2,nw_dst=10.0.3.2,actions=mod_dl_src:0A:00:0F:FE:00:02,mod_dl_dst:0A:00:0F:01:00:04,output=2
-$offctl add-flow s3 \
+$ofctl add-flow s3 \
     in_port=4,ip,nw_src=10.0.6.2,nw_dst=10.0.3.2,actions=mod_dl_src:0A:00:03:01:00:01,mod_dl_dst:0A:00:03:02:00:00,output=1
 
+# Print the flows installed in each switch
+for switch in s0 s1 s2 s3 s4 s6;
+do
+    echo "Flows installed in $switch:"
+    $ofctl dump-flows $switch
+    echo ""
+done
 
 
 
